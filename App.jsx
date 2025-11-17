b3b4a50f7f35372b18bc6af06f2c952bc6ab4d1a
sai1919t <valmikisaikrishna260@gmail.com>
2025-11-17 21:49:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AuthButtons from "./components/AuthButtons";
+
+export default function App() {
+  return (
+    /* full-screen center container */
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <AuthButtons />
+    </div>
+  );
+}
